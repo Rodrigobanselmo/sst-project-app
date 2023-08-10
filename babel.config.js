5f67174fb39fefcb0370ce53ecb674f20f6ac3b8
Rodrigo Barbosa Anselmo <rodrigo@MacBook-Pro-de-Rodrigo.local>
2023-08-10 13:40:08 -0300

    feat: char list observable wmdb

--- babel.config.js
+++ babel.config.js
@@ -1,45 +1,45 @@
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      "@babel/plugin-proposal-export-namespace-from",
-      "react-native-reanimated/plugin",
-      ["@babel/plugin-proposal-decorators", { "legacy": true }],
-      [
-        "module:react-native-dotenv",
-        {
-          "envName": "APP_ENV",
-          "moduleName": "@env",
-          "path": ".env",
-          "blocklist": null,
-          "allowlist": null,
-          "safe": false,
-          "allowUndefined": false,
-          "verbose": false
-        }
-      ],
-      [
-        "module-resolver",
-        {
-          root: ["./src"],
-          alias: {
-            "@interfaces": "./src/interfaces",
-            "@assets": "./src/assets",
-            "@components": "./src/components",
-            "@screens": "./src/screens",
-            "@libs": "./src/libs",
-            "@utils": "./src/utils",
-            "@services": "./src/services",
-            "@constants": "./src/constants",
-            "@hooks": "./src/hooks",
-            "@contexts": "./src/contexts",
-            "@routes": "./src/routes",
-            '@theme': './src/theme',
-            '@routes': './src/routes'
-          },
-        },
-      ],
-    ],
-  };
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            '@babel/plugin-proposal-export-namespace-from',
+            'react-native-reanimated/plugin',
+            ['@babel/plugin-proposal-decorators', { legacy: true }],
+            [
+                'module:react-native-dotenv',
+                {
+                    envName: 'APP_ENV',
+                    moduleName: '@env',
+                    path: '.env',
+                    blocklist: null,
+                    allowlist: null,
+                    safe: false,
+                    allowUndefined: false,
+                    verbose: false,
+                },
+            ],
+            [
+                'module-resolver',
+                {
+                    root: ['./src'],
+                    alias: {
+                        '@interfaces': './src/interfaces',
+                        '@assets': './src/assets',
+                        '@components': './src/components',
+                        '@screens': './src/screens',
+                        '@libs': './src/libs',
+                        '@utils': './src/utils',
+                        '@services': './src/services',
+                        '@constants': './src/constants',
+                        '@hooks': './src/hooks',
+                        '@repositories': './src/repositories',
+                        '@contexts': './src/contexts',
+                        '@theme': './src/theme',
+                        '@routes': './src/routes',
+                    },
+                },
+            ],
+        ],
+    };
 };
